71182526f1258e9772c1d175eac39b8e897bb5d8
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-10 18:36:41 -0400

    initial commit, cleaned up CRA boiler

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Galaxy Lens</h1>
+        <h2>Images from the Cosmos</h2>
+        <h3>Brought to you by NASA</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
