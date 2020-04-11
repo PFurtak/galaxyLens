0a1dda5be5b23c78d87667691fd49a6e8db20b93
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-10 21:20:15 -0400

    brought in CSS library, styled cards and search form

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 const Card = () => {
   return (
     <div>
-      <ul>
-        <li>Image</li>
-        <li>Title</li>
-        <li>Date</li>
-        <li>Caption</li>
-      </ul>
+      <div className='container'>
+        <div className='card text-center'>
+          <img src='' alt='' className='round-img' style={{ width: '60px' }} />
+          <h3>Title</h3>
+          <div>
+            <ul>
+              <li>Date</li>
+              <li>Caption</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
