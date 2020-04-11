e866f44512ef5501e49be8d404762aa001a65e12
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-10 20:45:36 -0400

    Created static card component pre-styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Search from './components/Search';
+import Card from './components/Card';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <h3>Brought to you by NASA</h3>
       </header>
       <Search />
+      <Card />
     </div>
   );
 }
