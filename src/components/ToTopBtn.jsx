d07570c0916b48d6baf85058d188835308deb7d0
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-13 12:58:20 -0400

    Created class based component for scroll to top button, browser lint error is gone. Will refactor to hooks

--- src/components/ToTopBtn.jsx
+++ src/components/ToTopBtn.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ToTopBtn = () => {
-  return (
-    <div style={topStyle}>
-      <a href='#'>
-        <i className='far fa-arrow-alt-circle-up text-light fa-2x'></i>
-      </a>
-    </div>
-  );
-};
+export default class ToTopBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      is_visible: false,
+    };
+  }
+
+  componentDidMount() {
+    let scrollComponent = this;
+    document.addEventListener('scroll', function (e) {
+      scrollComponent.toggleVisibility();
+    });
+  }
+
+  toggleVisibility() {
+    if (window.pageYOffset > 300) {
+      this.setState({
+        is_visible: true,
+      });
+    } else {
+      this.setState({
+        is_visible: false,
+      });
+    }
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  render() {
+    const { is_visible } = this.state;
+    return (
+      <div className='scroll-to-top'>
+        {is_visible && (
+          <div onClick={() => this.scrollToTop()} style={topStyle}>
+            <i className='far fa-arrow-alt-circle-up text-light fa-2x'></i>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 const topStyle = {
   position: 'fixed',
   bottom: '15px',
   right: '25px',
 };
-
-export default ToTopBtn;
