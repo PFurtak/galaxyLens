cceb48574c3322f50c33d730808433c7de33cf8f
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-13 20:03:44 -0400

    fixed improper html element nesting in the about component

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -26,37 +26,39 @@ export const About = () => {
           </a>
         </strong>
       </p>
-
       <br />
       <hr />
       <br />
       <h2>Credits</h2>
       <br />
+      <h3>API</h3>
       <p>
-        <h3>API</h3>Data is brought to you by the North American Space
-        Association image library. More information on the API can be found at
+        Data is brought to you by the North American Space Association image
+        library. More information on the API can be found at
         <a className='text-light' href='https://api.nasa.gov/'>
           {' '}
           api.nasa.gov
         </a>
       </p>
       <br />
+      <h3>CSS</h3>
       <p>
-        <h3>CSS</h3>CSS library provided by Brad Traversy. You can find his work
-        at{' '}
+        CSS library provided by Brad Traversy. You can find his work at{' '}
         <a className='text-light' href='https://github.com/bradtraversy'>
           github.com/bradtraversy
         </a>
       </p>
       <br />
+      <h3>GIFs</h3>
       <p>
-        <h3>GIFs</h3>GIFs were sourced from giphy.com and created by Tony Babel.
-        His work can be found at{' '}
+        GIFs were sourced from giphy.com and created by Tony Babel. His work can
+        be found at{' '}
         <a className='text-light' href='https://giphy.com/tonybabel'>
           {' '}
           giphy.com/tonybabel
         </a>
       </p>
+      <br />
     </div>
   );
 };
